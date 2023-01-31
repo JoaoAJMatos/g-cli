6b9e99c9449a533acbb4a603fbfac10ac98232d5
JoaoAJMatos <joaoafonsoj2004@gmail.com>
2023-01-31 14:25:06 +0000

    chore: Removes support for HSL colors

--- src/gcli.c
+++ src/gcli.c
@@ -1,144 +1,317 @@
-#include "../include/gcli.h"
+#include "../include/gcli/gcli.h"
 
 #define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
+#include "../include/thirdparty/stb_image.h"
 #define STB_IMAGE_RESIZE_IMPLEMENTATION
-#include "stb_image_resize.h"
+#include "../include/thirdparty/stb_image_resize.h"
 
-typedef struct {
-      int a;
-      int b;
-      int c;
-} color_t;
+#define CHAR_HEIGHT 32
+#define CHAR_WIDTH  16
 
-typedef struct {
-      color_t color;
-      int alpha;
-} color_alpha_t;
-
-static color_t rgb_to_color_t(rgb_t rgb_color) {
-      color_t color;
-      color.a = rgb_color.r;
-      color.b = rgb_color.g;
-      color.c = rgb_color.b;
-      return color;
-}
-
-static color_t hsl_to_color_t(hsl_t hsl_color) {
-      color_t color;
-      color.a = hsl_color.h;
-      color.b = hsl_color.s;
-      color.c = hsl_color.l;
-      return color;
-}
+static int colors[MAX_COLORS][3] = {
+      {0,0,0},
+      {128,0,0},
+      {0,128,0},
+      {128,128,0},
+      {0,0,128},
+      {128,0,128},
+      {0,128,128},
+      {192,192,192},
+      {128,128,128},
+      {255,0,0},
+      {0,255,0},
+      {255,255,0},
+      {0,0,255},
+      {255,0,255},
+      {0,255,255},
+      {255,255,255},
+      {0,0,0},
+      {0,0,95},
+      {0,0,135},
+      {0,0,175},
+      {0,0,215},
+      {0,0,255},
+      {0,95,0},
+      {0,95,95},
+      {0,95,135},
+      {0,95,175},
+      {0,95,215},
+      {0,95,255},
+      {0,135,0},
+      {0,135,95},
+      {0,135,135},
+      {0,135,175},
+      {0,135,215},
+      {0,135,255},
+      {0,175,0},
+      {0,175,95},
+      {0,175,135},
+      {0,175,175},
+      {0,175,215},
+      {0,175,255},
+      {0,215,0},
+      {0,215,95},
+      {0,215,135},
+      {0,215,175},
+      {0,215,215},
+      {0,215,255},
+      {0,255,0},
+      {0,255,95},
+      {0,255,135},
+      {0,255,175},
+      {0,255,215},
+      {0,255,255},
+      {95,0,0},
+      {95,0,95},
+      {95,0,135},
+      {95,0,175},
+      {95,0,215},
+      {95,0,255},
+      {95,95,0},
+      {95,95,95},
+      {95,95,135},
+      {95,95,175},
+      {95,95,215},
+      {95,95,255},
+      {95,135,0},
+      {95,135,95},
+      {95,135,135},
+      {95,135,175},
+      {95,135,215},
+      {95,135,255},
+      {95,175,0},
+      {95,175,95},
+      {95,175,135},
+      {95,175,175},
+      {95,175,215},
+      {95,175,255},
+      {95,215,0},
+      {95,215,95},
+      {95,215,135},
+      {95,215,175},
+      {95,215,215},
+      {95,215,255},
+      {95,255,0},
+      {95,255,95},
+      {95,255,135},
+      {95,255,175},
+      {95,255,215},
+      {95,255,255},
+      {135,0,0},
+      {135,0,95},
+      {135,0,135},
+      {135,0,175},
+      {135,0,215},
+      {135,0,255},
+      {135,95,0},
+      {135,95,95},
+      {135,95,135},
+      {135,95,175},
+      {135,95,215},
+      {135,95,255},
+      {135,135,0},
+      {135,135,95},
+      {135,135,135},
+      {135,135,175},
+      {135,135,215},
+      {135,135,255},
+      {135,175,0},
+      {135,175,95},
+      {135,175,135},
+      {135,175,175},
+      {135,175,215},
+      {135,175,255},
+      {135,215,0},
+      {135,215,95},
+      {135,215,135},
+      {135,215,175},
+      {135,215,215},
+      {135,215,255},
+      {135,255,0},
+      {135,255,95},
+      {135,255,135},
+      {135,255,175},
+      {135,255,215},
+      {135,255,255},
+      {175,0,0},
+      {175,0,95},
+      {175,0,135},
+      {175,0,175},
+      {175,0,215},
+      {175,0,255},
+      {175,95,0},
+      {175,95,95},
+      {175,95,135},
+      {175,95,175},
+      {175,95,215},
+      {175,95,255},
+      {175,135,0},
+      {175,135,95},
+      {175,135,135},
+      {175,135,175},
+      {175,135,215},
+      {175,135,255},
+      {175,175,0},
+      {175,175,95},
+      {175,175,135},
+      {175,175,175},
+      {175,175,215},
+      {175,175,255},
+      {175,215,0},
+      {175,215,95},
+      {175,215,135},
+      {175,215,175},
+      {175,215,215},
+      {175,215,255},
+      {175,255,0},
+      {175,255,95},
+      {175,255,135},
+      {175,255,175},
+      {175,255,215},
+      {175,255,255},
+      {215,0,0},
+      {215,0,95},
+      {215,0,135},
+      {215,0,175},
+      {215,0,215},
+      {215,0,255},
+      {215,95,0},
+      {215,95,95},
+      {215,95,135},
+      {215,95,175},
+      {215,95,215},
+      {215,95,255},
+      {215,135,0},
+      {215,135,95},
+      {215,135,135},
+      {215,135,175},
+      {215,135,215},
+      {215,135,255},
+      {215,175,0},
+      {215,175,95},
+      {215,175,135},
+      {215,175,175},
+      {215,175,215},
+      {215,175,255},
+      {215,215,0},
+      {215,215,95},
+      {215,215,135},
+      {215,215,175},
+      {215,215,215},
+      {215,215,255},
+      {215,255,0},
+      {215,255,95},
+      {215,255,135},
+      {215,255,175},
+      {215,255,215},
+      {215,255,255},
+      {255,0,0},
+      {255,0,95},
+      {255,0,135},
+      {255,0,175},
+      {255,0,215},
+      {255,0,255},
+      {255,95,0},
+      {255,95,95},
+      {255,95,135},
+      {255,95,175},
+      {255,95,215},
+      {255,95,255},
+      {255,135,0},
+      {255,135,95},
+      {255,135,135},
+      {255,135,175},
+      {255,135,215},
+      {255,135,255},
+      {255,175,0},
+      {255,175,95},
+      {255,175,135},
+      {255,175,175},
+      {255,175,215},
+      {255,175,255},
+      {255,215,0},
+      {255,215,95},
+      {255,215,135},
+      {255,215,175},
+      {255,215,215},
+      {255,215,255},
+      {255,255,0},
+      {255,255,95},
+      {255,255,135},
+      {255,255,175},
+      {255,255,215},
+      {255,255,255},
+      {8,8,8},
+      {18,18,18},
+      {28,28,28},
+      {38,38,38},
+      {48,48,48},
+      {58,58,58},
+      {68,68,68},
+      {78,78,78},
+      {88,88,88},
+      {98,98,98},
+      {108,108,108},
+      {118,118,118},
+      {128,128,128},
+      {138,138,138},
+      {148,148,148},
+      {158,158,158},
+      {168,168,168},
+      {178,178,178},
+      {188,188,188},
+      {198,198,198},
+      {208,208,208},
+      {218,218,218},
+      {228,228,228},
+      {238,238,238},
+};
 
 /**
- * @brief Loads the terminal colors from the file.
- * 
- * Returns the number of colors loaded or -1 if the file could not be opened.
- * 
- * @param filename char*
- * @param colors int[][3]
- * @param max_colors int 
- * @return int 
- */
-static int load_colors(char *filename, int colors[][3], int max_colors) {
-      int i = 0;
-      int r, g, b;
-      int scanf_result;
-
-      FILE *fp = fopen(filename, "r");
-      if (fp == NULL) return GCLI_ERR;
-
-      while (i < max_colors) {
-            scanf_result = fscanf(fp, "%d %d %d", &r, &g, &b);
-            if (scanf_result != 3) break;
-
-            colors[i][0] = r;
-            colors[i][1] = g;
-            colors[i][2] = b;
-            i++;
-      }
-      fclose(fp);
-      return i;
-}
-
-/**
- * @brief Converts a given RGB color to HSL
- * 
- * @param rgb_color rgb_t
- * @param hsl_color hsl_t* 
- */
-static void rgb_to_hsl(rgb_t rgb_color, hsl_t *hsl_color) {
-      float r01 = rgb_color.r/255.0f;
-      float g01 = rgb_color.g/255.0f;
-      float b01 = rgb_color.b/255.0f;
-
-      float cmax = r01;
-      float cmin = r01;
-
-      if (g01 > cmax) cmax = g01;
-      if (b01 > cmax) cmax = b01;
-
-      if (g01 < cmin) cmin = g01;
-      if (b01 < cmin) cmin = b01;
-
-      float delta = cmax - cmin;
-      float epsilon = 1e-6;
-      float hf = 0;
-
-      if (delta < epsilon) hf = 0;
-      else if (cmax == r01) hf = 60.0f*fmod((g01 - b01)/delta, 6.0f);
-      else if (cmax == g01) hf = 60.0f*((b01 - r01)/delta + 2);
-      else if (cmax == b01) hf = 60.0f*((r01 - g01)/delta + 4);
-      else unreachable();
-
-      float lf = (cmax + cmin)/2;
-
-      float sf = 0;
-      if (delta < epsilon) sf = 0;
-      else sf = delta/(1 - fabsf(2*lf - 1));
-
-      hsl_color->h = fmodf(fmodf(hf, 360.0f) + 360.0f, 360.0f);
-      hsl_color->s = sf*100.0f;
-      hsl_color->l = lf*100.0f;
-}
-
-/**
- * @brief Returns the distance between two colors.
+ * @brief Returns the distance between two RGB colors.
  * 
  * @param color
  * @param color
  * @return int 
  */
-static int distance_256(color_t color_one, color_t color_two) {
-      int distance_a = color_one.a - color_two.a;
-      int distance_r = color_one.b - color_two.b;
-      int distance_g = color_one.c - color_two.c;
-      return distance_a*distance_a + distance_r*distance_r + distance_g*distance_g;
+static int distance_256(rgb_t color_one, rgb_t color_two) {
+      int distance_r = color_one.r - color_two.r;
+      int distance_g = color_one.g - color_two.g;
+      int distance_b = color_one.b - color_two.b;
+      return distance_r*distance_r + distance_g*distance_g + distance_b*distance_b;
 }
 
 /**
- * @brief Finds the closest ANSI color to the given color.
+ * @brief Finds the closest ANSI color to the given RGB color.
  * 
  * Returns the index of the closest color found.
  * 
  * @return int 
  */
-static int find_closest_color_index(struct gcli* gcli, color_t color, color_family_t color_family) {
+static int find_closest_color_index(struct gcli* gcli, rgb_t color) {
       int index = 0;
       for (int i = 0; i < MAX_COLORS; i++) {
-            if (color_family == RGB) {
-                  if (distance_256(color, rgb_to_color_t(gcli->rgb_colors[i])) < distance_256(color, rgb_to_color_t(gcli->rgb_colors[index]))) {
-                        index = i;
-                  }
-            } else if (color_family == HSL) {
-                  if (distance_256(color, hsl_to_color_t(gcli->hsl_colors[i])) < distance_256(color, hsl_to_color_t(gcli->hsl_colors[index]))) {
-                        index = i;
-                  }
+            if (distance_256(color, gcli->colors[i]) < distance_256(color, gcli->colors[index])) {
+                  index = i;
             }
       }
       return index;
+}
+
+/**
+ * @brief Creates a new RGB color.
+ * 
+ * @param r 
+ * @param g 
+ * @param b 
+ * @return rgb_t 
+ */
+static rgb_t gcli_rgb(int r, int g, int b) {
+      rgb_t color;
+      color.r = r;
+      color.g = g;
+      color.b = b;
+      return color;
 }
 
 /* ============================================================== */
@@ -147,41 +320,33 @@ static int find_closest_color_index(struct gcli* gcli, color_t color, color_fami
 /**
  * @brief Initializes the gcli library.
  * 
- * Loads the terminal colors from the files and stores them in the gcli struct.
- * 
  * @param gcli struct gcli*
  * @return Errno
  */
 Errno gcli_init(struct gcli* gcli) {
-      int rgb_colors[MAX_COLORS][3];
-      int hsl_colors[MAX_COLORS][3];
-
-      int rgb_colors_count = load_colors("../common/rgb256.txt", rgb_colors, MAX_COLORS);
-      int hsl_colors_count = load_colors("../common/hsl256.txt", hsl_colors, MAX_COLORS);
-
-      if (rgb_colors_count == -1 || hsl_colors_count == -1) {
-            gcli->last_error = "Could not open colors file.";
-            return GCLI_ERR;
-      }
-
-      for (int i = 0; i < rgb_colors_count; i++) {
-            gcli->rgb_colors[i].r = rgb_colors[i][0];
-            gcli->rgb_colors[i].g = rgb_colors[i][1];
-            gcli->rgb_colors[i].b = rgb_colors[i][2];
-      }
-
-      for (int i = 0; i < hsl_colors_count; i++) {
-            gcli->hsl_colors[i].h = hsl_colors[i][0];
-            gcli->hsl_colors[i].s = hsl_colors[i][1];
-            gcli->hsl_colors[i].l = hsl_colors[i][2];
+      for (int i = 0; i < MAX_COLORS; i++) {
+            gcli->colors[i] = gcli_rgb(colors[i][0], colors[i][1], colors[i][2]);
       }
 
       gcli->width = DEFAULT_WIDTH;
       gcli->height = 0;
       gcli->resized_width = DEFAULT_WIDTH;
       gcli->resized_height = 0;
-
       return GCLI_OK;
+}
+
+// TODO: Inline this to see if it improves performance.
+static void print_line(struct gcli gcli, size_t line_size, rgb_t* line) {
+      char *buffer = malloc(32 * line_size * sizeof(char));
+      char temp[1024] = {0};
+
+      for (size_t i = 0; i < line_size; i++) {
+            int index = find_closest_color_index(&gcli, line[i]);
+            sprintf(temp, "\e[48;5;%dm  ", index);
+            strcat(buffer, temp);
+      }
+
+      printf("%s", buffer);
 }
 
 
@@ -193,7 +358,7 @@ Errno gcli_init(struct gcli* gcli) {
  * @param color_type 
  * @return Errno 
  */
-Errno gcli_img_to_terminal(struct gcli* gcli, const char* filename, color_family_t color_type, int resized_width) {
+Errno gcli_img_to_terminal(struct gcli* gcli, const char* filename, int resized_width, int resized_height) {
       uint32_t *pixels = NULL;
       uint32_t *resized_pixels = NULL;
 
@@ -204,7 +369,7 @@ Errno gcli_img_to_terminal(struct gcli* gcli, const char* filename, color_family
       }
 
       gcli->resized_width = resized_width;
-      gcli->resized_height = gcli->height * gcli->resized_width / gcli->width;
+      gcli->resized_height = resized_height;
 
       resized_pixels = malloc(gcli->resized_width * gcli->resized_height * sizeof(uint32_t));
       if (resized_pixels == NULL) {
@@ -215,29 +380,21 @@ Errno gcli_img_to_terminal(struct gcli* gcli, const char* filename, color_family
       stbir_resize_uint8((uint8_t*)pixels, gcli->width, gcli->height, 0, (uint8_t*)resized_pixels, gcli->resized_width, gcli->resized_height, 0, 4);
 
       for (int y = 0; y < gcli->resized_height; y++) {
+            rgb_t line[gcli->resized_width];
+
             for (int x = 0; x < gcli->resized_width; x++) {
                   uint32_t pixel = resized_pixels[y*gcli->resized_width + x];
-                  color_alpha_t color = { 
-                        .color.a = (pixel >> 24) & 0xFF, 
-                        .color.b = (pixel >> 16) & 0xFF, 
-                        .color.c = (pixel >> 8) & 0xFF, 
-                        .alpha = pixel & 0xFF 
-                  };
-
+                  
                   rgb_t rgb_color = {
-                        .r = color.alpha * color.color.a / 255,
-                        .g = color.alpha * color.color.b / 255,
-                        .b = color.alpha * color.color.c / 255
+                        .r = (pixel & 0xFF) * ((pixel >> 24) & 0xFF) / 255,
+                        .g = (pixel & 0xFF) * ((pixel >> 16) & 0xFF) / 255,
+                        .b = (pixel & 0xFF) * ((pixel >> 8) & 0xFF)  / 255
                   };
 
-                  if (color_type == RGB) {
-                        print_color(find_closest_color_index(gcli, rgb_to_color_t(rgb_color), RGB));
-                  } else if (color_type == HSL) {
-                        hsl_t hsl_color;
-                        rgb_to_hsl(rgb_color, &hsl_color);
-                        print_color(find_closest_color_index(gcli, hsl_to_color_t(hsl_color), HSL));
-                  }
+                  line[x] = rgb_color;
+                  print_color(find_closest_color_index(gcli, rgb_color));
             }
+
             printf("\e[0m\n");
       }
       printf("\e[0m\n");
@@ -251,3 +408,25 @@ Errno gcli_img_to_terminal(struct gcli* gcli, const char* filename, color_family
 void gcli_print_last_error(struct gcli* gcli) {
       printf("%s\n", gcli->last_error);
 }
+
+
+/* ============================================================== */
+
+/**
+ * @brief Creates a new canvas.
+ * 
+ * @param name 
+ * @param pixels 
+ * @param width 
+ * @param height 
+ * @return gcli_canvas_t 
+ */
+gcli_canvas_t gcli_canvas(const char* name, uint32_t pixels[], size_t width, size_t height){
+      gcli_canvas_t canvas;
+      canvas.name   = name;
+      canvas.pixels = pixels;
+      canvas.width  = width;
+      canvas.height = height;
+      return canvas;
+}
+
